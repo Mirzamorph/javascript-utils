d539dabd0f88964364252b324860aa0a1a83afc4
Rustam <mirzamorf@yandex.ru>
2021-03-01 19:38:22 +0300

    finished cookie function

--- cookie.js
+++ cookie.js
@@ -6,10 +6,44 @@ function cookieGet(key) {
         return null
     }
 
-    const res =  cookieStr
+    const res = cookieStr
         .split(';')
         .find(keyValue => keyValue.includes(`${encodedKey}=`))
         .split('=', 2)[1]
 
     return window.decodeURI(res)
+}
+
+function cookieSet(key, value, options = {}) {
+    const encodedKey = window.encodeURIComponent(key)
+    const encodedValue = window.encodeURIComponent(value)
+
+    let updatedCookie = `${encodedKey}=${encodedValue}`
+
+    options = {
+        path: '/',
+        ...options
+    }
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    for (let optionKey in options) {
+        if (options.hasOwnProperty(optionKey)) {
+            updatedCookie += "; " + optionKey;
+            let optionValue = options[optionKey];
+            if (optionValue !== true) {
+                updatedCookie += "=" + optionValue;
+            }
+        }
+      }
+
+    document.cookie = updatedCookie
+}
+
+function cookieRemove(key) {
+    cookieSet(key, '', {
+        'max-age': -1
+    })
 }
